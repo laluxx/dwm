064ebfea787073e2d9be19e45c81667c8b4350ac
laluxx <lorenzoaddamo10@gmail.com>
2024-11-25 10:02:55 +0100

    ewmhtags

--- dwm.c
+++ dwm.c
@@ -54,17 +54,29 @@
 #define WIDTH(X)                ((X)->w + 2 * (X)->bw)
 #define HEIGHT(X)               ((X)->h + 2 * (X)->bw)
 #define TAGMASK                 ((1 << LENGTH(tags)) - 1)
+#define TAGSLENGTH              (LENGTH(tags))
 #define TEXTW(X)                (drw_fontset_getwidth(drw, (X)) + lrpad)
 
 /* enums */
+/* enum { CurNormal, CurResize, CurMove, CurLast }; /\* cursor *\/ */
+/* enum { SchemeNorm, SchemeSel }; /\* color schemes *\/ */
+/* enum { NetSupported, NetWMName, NetWMState, NetWMCheck, */
+/*        NetWMFullscreen, NetActiveWindow, NetWMWindowType, */
+/*        NetWMWindowTypeDialog, NetClientList, NetClientInfo, NetLast }; /\* EWMH atoms *\/ */
+/* enum { WMProtocols, WMDelete, WMState, WMTakeFocus, WMLast }; /\* default atoms *\/ */
+/* enum { ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, */
+/*        ClkClientWin, ClkRootWin, ClkLast }; /\* clicks *\/ */
+
 enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
 enum { SchemeNorm, SchemeSel }; /* color schemes */
 enum { NetSupported, NetWMName, NetWMState, NetWMCheck,
-       NetWMFullscreen, NetActiveWindow, NetWMWindowType,
-       NetWMWindowTypeDialog, NetClientList, NetClientInfo, NetLast }; /* EWMH atoms */
+    NetWMFullscreen, NetActiveWindow, NetWMWindowType,
+    NetWMWindowTypeDialog, NetClientList, NetDesktopNames, 
+    NetDesktopViewport, NetNumberOfDesktops, NetCurrentDesktop, 
+    NetClientInfo, NetLast }; /* EWMH atoms */
 enum { WMProtocols, WMDelete, WMState, WMTakeFocus, WMLast }; /* default atoms */
 enum { ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
-       ClkClientWin, ClkRootWin, ClkLast }; /* clicks */
+    ClkClientWin, ClkRootWin, ClkLast }; /* clicks */
 
 typedef union {
 	int i;
@@ -218,6 +230,7 @@ static void toggleview(const Arg *arg);
 static void unfocus(Client *c, int setfocus);
 static void unmanage(Client *c, int destroyed);
 static void unmapnotify(XEvent *e);
+static void updatecurrentdesktop(void);
 static void updatebarpos(Monitor *m);
 static void updatebars(void);
 static void updateclientlist(void);
@@ -238,6 +251,14 @@ static int xerror(Display *dpy, XErrorEvent *ee);
 static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void zoom(const Arg *arg);
+
+static void setcurrentdesktop(void);
+static void setdesktopnames(void);
+static void setnumdesktops(void);
+static void setviewport(void);
+static void updatecurrentdesktop(void);
+
+
 
 /* variables */
 static const char broken[] = "broken";
@@ -1408,7 +1429,7 @@ scan(void)
 	if (XQueryTree(dpy, root, &d1, &d2, &wins, &num)) {
 		for (i = 0; i < num; i++) {
 			if (!XGetWindowAttributes(dpy, wins[i], &wa)
-			|| wa.override_redirect || XGetTransientForHint(dpy, wins[i], &d1))
+                || wa.override_redirect || XGetTransientForHint(dpy, wins[i], &d1))
 				continue;
 			if (wa.map_state == IsViewable || getstate(wins[i]) == IconicState)
 				manage(wins[i], &wa);
@@ -1417,13 +1438,40 @@ scan(void)
 			if (!XGetWindowAttributes(dpy, wins[i], &wa))
 				continue;
 			if (XGetTransientForHint(dpy, wins[i], &d1)
-			&& (wa.map_state == IsViewable || getstate(wins[i]) == IconicState))
+                && (wa.map_state == IsViewable || getstate(wins[i]) == IconicState))
 				manage(wins[i], &wa);
 		}
 		if (wins)
 			XFree(wins);
 	}
 }
+
+/* void */
+/* scan(void) */
+/* { */
+/* 	unsigned int i, num; */
+/* 	Window d1, d2, *wins = NULL; */
+/* 	XWindowAttributes wa; */
+
+/* 	if (XQueryTree(dpy, root, &d1, &d2, &wins, &num)) { */
+/* 		for (i = 0; i < num; i++) { */
+/* 			if (!XGetWindowAttributes(dpy, wins[i], &wa) */
+/* 			|| wa.override_redirect || XGetTransientForHint(dpy, wins[i], &d1)) */
+/* 				continue; */
+/* 			if (wa.map_state == IsViewable || getstate(wins[i]) == IconicState) */
+/* 				manage(wins[i], &wa); */
+/* 		} */
+/* 		for (i = 0; i < num; i++) { /\* now the transients *\/ */
+/* 			if (!XGetWindowAttributes(dpy, wins[i], &wa)) */
+/* 				continue; */
+/* 			if (XGetTransientForHint(dpy, wins[i], &d1) */
+/* 			&& (wa.map_state == IsViewable || getstate(wins[i]) == IconicState)) */
+/* 				manage(wins[i], &wa); */
+/* 		} */
+/* 		if (wins) */
+/* 			XFree(wins); */
+/* 	} */
+/* } */
 
 void
 sendmon(Client *c, Monitor *m)
@@ -1450,6 +1498,30 @@ setclientstate(Client *c, long state)
 	XChangeProperty(dpy, c->win, wmatom[WMState], wmatom[WMState], 32,
 		PropModeReplace, (unsigned char *)data, 2);
 }
+void
+setcurrentdesktop(void){
+	long data[] = { 0 };
+	XChangeProperty(dpy, root, netatom[NetCurrentDesktop], XA_CARDINAL, 32, PropModeReplace, (unsigned char *)data, 1);
+}
+
+void setdesktopnames(void) {
+    XTextProperty text;
+    char **taglist = malloc(sizeof(char*) * LENGTH(tags));
+    for (int i = 0; i < LENGTH(tags); i++)
+        taglist[i] = (char *)tags[i];
+    
+    if (Xutf8TextListToTextProperty(dpy, taglist, LENGTH(tags), XUTF8StringStyle, &text) == Success) {
+        XSetTextProperty(dpy, root, &text, netatom[NetDesktopNames]);
+        XFree(text.value);
+    }
+    free(taglist);
+}
+
+/* void setdesktopnames(void){ */
+/* 	XTextProperty text; */
+/* 	Xutf8TextListToTextProperty(dpy, tags, TAGSLENGTH, XUTF8StringStyle, &text); */
+/* 	XSetTextProperty(dpy, root, &text, netatom[NetDesktopNames]); */
+/* } */
 
 int
 sendevent(Client *c, Atom proto)
@@ -1474,6 +1546,12 @@ sendevent(Client *c, Atom proto)
 		XSendEvent(dpy, c->win, False, NoEventMask, &ev);
 	}
 	return exists;
+}
+
+void
+setnumdesktops(void){
+	long data[] = { TAGSLENGTH };
+	XChangeProperty(dpy, root, netatom[NetNumberOfDesktops], XA_CARDINAL, 32, PropModeReplace, (unsigned char *)data, 1);
 }
 
 void
@@ -1569,6 +1647,7 @@ setup(void)
 	lrpad = drw->fonts->h;
 	bh = drw->fonts->h + 2;
 	updategeom();
+
 	/* init atoms */
 	utf8string = XInternAtom(dpy, "UTF8_STRING", False);
 	wmatom[WMProtocols] = XInternAtom(dpy, "WM_PROTOCOLS", False);
@@ -1584,31 +1663,47 @@ setup(void)
 	netatom[NetWMWindowType] = XInternAtom(dpy, "_NET_WM_WINDOW_TYPE", False);
 	netatom[NetWMWindowTypeDialog] = XInternAtom(dpy, "_NET_WM_WINDOW_TYPE_DIALOG", False);
 	netatom[NetClientList] = XInternAtom(dpy, "_NET_CLIENT_LIST", False);
+	netatom[NetDesktopViewport] = XInternAtom(dpy, "_NET_DESKTOP_VIEWPORT", False);
+	netatom[NetNumberOfDesktops] = XInternAtom(dpy, "_NET_NUMBER_OF_DESKTOPS", False);
+	netatom[NetCurrentDesktop] = XInternAtom(dpy, "_NET_CURRENT_DESKTOP", False);
+	netatom[NetDesktopNames] = XInternAtom(dpy, "_NET_DESKTOP_NAMES", False);
 	netatom[NetClientInfo] = XInternAtom(dpy, "_NET_CLIENT_INFO", False);
+
 	/* init cursors */
 	cursor[CurNormal] = drw_cur_create(drw, XC_left_ptr);
 	cursor[CurResize] = drw_cur_create(drw, XC_sizing);
 	cursor[CurMove] = drw_cur_create(drw, XC_fleur);
+
 	/* init appearance */
 	scheme = ecalloc(LENGTH(colors), sizeof(Clr *));
 	for (i = 0; i < LENGTH(colors); i++)
 		scheme[i] = drw_scm_create(drw, colors[i], 3);
+
 	/* init bars */
 	updatebars();
 	updatestatus();
+
 	/* supporting window for NetWMCheck */
 	wmcheckwin = XCreateSimpleWindow(dpy, root, 0, 0, 1, 1, 0, 0, 0);
 	XChangeProperty(dpy, wmcheckwin, netatom[NetWMCheck], XA_WINDOW, 32,
-		PropModeReplace, (unsigned char *) &wmcheckwin, 1);
+                    PropModeReplace, (unsigned char *) &wmcheckwin, 1);
 	XChangeProperty(dpy, wmcheckwin, netatom[NetWMName], utf8string, 8,
-		PropModeReplace, (unsigned char *) "dwm", 3);
+                    PropModeReplace, (unsigned char *) "dwm", 3);
 	XChangeProperty(dpy, root, netatom[NetWMCheck], XA_WINDOW, 32,
-		PropModeReplace, (unsigned char *) &wmcheckwin, 1);
+                    PropModeReplace, (unsigned char *) &wmcheckwin, 1);
+
 	/* EWMH support per view */
 	XChangeProperty(dpy, root, netatom[NetSupported], XA_ATOM, 32,
-		PropModeReplace, (unsigned char *) netatom, NetLast);
+                    PropModeReplace, (unsigned char *) netatom, NetLast);
+	setnumdesktops();
+	setcurrentdesktop();
+	setdesktopnames();
+	setviewport();
+
+	/* clear client list properties */
 	XDeleteProperty(dpy, root, netatom[NetClientList]);
 	XDeleteProperty(dpy, root, netatom[NetClientInfo]);
+
 	/* select events */
 	wa.cursor = cursor[CurNormal]->cursor;
 	wa.event_mask = SubstructureRedirectMask|SubstructureNotifyMask
@@ -1618,6 +1713,94 @@ setup(void)
 	XSelectInput(dpy, root, wa.event_mask);
 	grabkeys();
 	focus(NULL);
+}
+
+/* void */
+/* setup(void) */
+/* { */
+/* 	int i; */
+/* 	XSetWindowAttributes wa; */
+/* 	Atom utf8string; */
+/* 	struct sigaction sa; */
+
+/* 	/\* do not transform children into zombies when they terminate *\/ */
+/* 	sigemptyset(&sa.sa_mask); */
+/* 	sa.sa_flags = SA_NOCLDSTOP | SA_NOCLDWAIT | SA_RESTART; */
+/* 	sa.sa_handler = SIG_IGN; */
+/* 	sigaction(SIGCHLD, &sa, NULL); */
+
+/* 	/\* clean up any zombies (inherited from .xinitrc etc) immediately *\/ */
+/* 	while (waitpid(-1, NULL, WNOHANG) > 0); */
+
+/* 	/\* init screen *\/ */
+/* 	screen = DefaultScreen(dpy); */
+/* 	sw = DisplayWidth(dpy, screen); */
+/* 	sh = DisplayHeight(dpy, screen); */
+/* 	root = RootWindow(dpy, screen); */
+/* 	drw = drw_create(dpy, screen, root, sw, sh); */
+/* 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts))) */
+/* 		die("no fonts could be loaded."); */
+/* 	lrpad = drw->fonts->h; */
+/* 	bh = drw->fonts->h + 2; */
+/* 	updategeom(); */
+/* 	/\* init atoms *\/ */
+/* 	utf8string = XInternAtom(dpy, "UTF8_STRING", False); */
+/* 	wmatom[WMProtocols] = XInternAtom(dpy, "WM_PROTOCOLS", False); */
+/* 	wmatom[WMDelete] = XInternAtom(dpy, "WM_DELETE_WINDOW", False); */
+/* 	wmatom[WMState] = XInternAtom(dpy, "WM_STATE", False); */
+/* 	wmatom[WMTakeFocus] = XInternAtom(dpy, "WM_TAKE_FOCUS", False); */
+/* 	netatom[NetActiveWindow] = XInternAtom(dpy, "_NET_ACTIVE_WINDOW", False); */
+/* 	netatom[NetSupported] = XInternAtom(dpy, "_NET_SUPPORTED", False); */
+/* 	netatom[NetWMName] = XInternAtom(dpy, "_NET_WM_NAME", False); */
+/* 	netatom[NetWMState] = XInternAtom(dpy, "_NET_WM_STATE", False); */
+/* 	netatom[NetWMCheck] = XInternAtom(dpy, "_NET_SUPPORTING_WM_CHECK", False); */
+/* 	netatom[NetWMFullscreen] = XInternAtom(dpy, "_NET_WM_STATE_FULLSCREEN", False); */
+/* 	netatom[NetWMWindowType] = XInternAtom(dpy, "_NET_WM_WINDOW_TYPE", False); */
+/* 	netatom[NetWMWindowTypeDialog] = XInternAtom(dpy, "_NET_WM_WINDOW_TYPE_DIALOG", False); */
+/* 	netatom[NetClientList] = XInternAtom(dpy, "_NET_CLIENT_LIST", False); */
+/* 	netatom[NetClientInfo] = XInternAtom(dpy, "_NET_CLIENT_INFO", False); */
+/* 	/\* init cursors *\/ */
+/* 	cursor[CurNormal] = drw_cur_create(drw, XC_left_ptr); */
+/* 	cursor[CurResize] = drw_cur_create(drw, XC_sizing); */
+/* 	cursor[CurMove] = drw_cur_create(drw, XC_fleur); */
+/* 	/\* init appearance *\/ */
+/* 	scheme = ecalloc(LENGTH(colors), sizeof(Clr *)); */
+/* 	for (i = 0; i < LENGTH(colors); i++) */
+/* 		scheme[i] = drw_scm_create(drw, colors[i], 3); */
+/* 	/\* init bars *\/ */
+/* 	updatebars(); */
+/* 	updatestatus(); */
+/* 	/\* supporting window for NetWMCheck *\/ */
+/* 	wmcheckwin = XCreateSimpleWindow(dpy, root, 0, 0, 1, 1, 0, 0, 0); */
+/* 	XChangeProperty(dpy, wmcheckwin, netatom[NetWMCheck], XA_WINDOW, 32, */
+/* 		PropModeReplace, (unsigned char *) &wmcheckwin, 1); */
+/* 	XChangeProperty(dpy, wmcheckwin, netatom[NetWMName], utf8string, 8, */
+/* 		PropModeReplace, (unsigned char *) "dwm", 3); */
+/* 	XChangeProperty(dpy, root, netatom[NetWMCheck], XA_WINDOW, 32, */
+/* 		PropModeReplace, (unsigned char *) &wmcheckwin, 1); */
+/* 	/\* EWMH support per view *\/ */
+/* 	XChangeProperty(dpy, root, netatom[NetSupported], XA_ATOM, 32, */
+/* 		PropModeReplace, (unsigned char *) netatom, NetLast); */
+/* 	setnumdesktops(); */
+/* 	setcurrentdesktop(); */
+/* 	setdesktopnames(); */
+/* 	setviewport(); */
+/* 	XDeleteProperty(dpy, root, netatom[NetClientList]); */
+/* 	XDeleteProperty(dpy, root, netatom[NetClientInfo]); */
+/* 	/\* select events *\/ */
+/* 	wa.cursor = cursor[CurNormal]->cursor; */
+/* 	wa.event_mask = SubstructureRedirectMask|SubstructureNotifyMask */
+/* 		|ButtonPressMask|PointerMotionMask|EnterWindowMask */
+/* 		|LeaveWindowMask|StructureNotifyMask|PropertyChangeMask; */
+/* 	XChangeWindowAttributes(dpy, root, CWEventMask|CWCursor, &wa); */
+/* 	XSelectInput(dpy, root, wa.event_mask); */
+/* 	grabkeys(); */
+/* 	focus(NULL); */
+/* } */
+void
+setviewport(void){
+	long data[] = { 0, 0 };
+	XChangeProperty(dpy, root, netatom[NetDesktopViewport], XA_CARDINAL, 32, PropModeReplace, (unsigned char *)data, 2);
 }
 
 void
@@ -1765,6 +1948,7 @@ toggletag(const Arg *arg)
 		focus(NULL);
 		arrange(selmon);
 	}
+	updatecurrentdesktop();
 }
 
 void
@@ -1777,6 +1961,7 @@ toggleview(const Arg *arg)
 		focus(NULL);
 		arrange(selmon);
 	}
+	updatecurrentdesktop();
 }
 
 void
@@ -1880,6 +2065,27 @@ updateclientlist(void)
 				XA_WINDOW, 32, PropModeAppend,
 				(unsigned char *) &(c->win), 1);
 }
+
+void updatecurrentdesktop(void){
+    long rawdata[] = { selmon->tagset[selmon->seltags] };
+    int i = 0;
+    while(*rawdata >> (i + 1)) {
+        i++;
+    }
+    long data[] = { i };
+    XChangeProperty(dpy, root, netatom[NetCurrentDesktop], XA_CARDINAL,
+                    32, PropModeReplace, (unsigned char *)data, 1);
+}
+
+/* void updatecurrentdesktop(void){ */
+/* 	long rawdata[] = { selmon->tagset[selmon->seltags] }; */
+/* 	int i=0; */
+/* 	while(*rawdata >> i+1){ */
+/* 		i++; */
+/* 	} */
+/* 	long data[] = { i }; */
+/* 	XChangeProperty(dpy, root, netatom[NetCurrentDesktop], XA_CARDINAL, 32, PropModeReplace, (unsigned char *)data, 1); */
+/* } */
 
 int
 updategeom(void)
@@ -2122,6 +2328,7 @@ view(const Arg *arg)
 		selmon->tagset[selmon->seltags] = arg->ui & TAGMASK;
 	focus(NULL);
 	arrange(selmon);
+	updatecurrentdesktop();
 }
 
 Client *
